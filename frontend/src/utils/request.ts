5238fa17e2eb86265d282673fba6d2868777b408
raziel.araujo <raziel_jose@hotmail.com>
2022-11-18 11:29:20 -0300

    Ajeitando código do request.ts

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "https://dsmeta-raziel.herokuapp.com/";
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "https://dsmeta-raziel.herokuapp.com";
